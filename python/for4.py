dfb32c68d93968f9f58ecb2dbd4eec6a73b787a7
kimyeaeun96 <mangokim96@gmail.com>
2022-07-08 16:23:14 +0900

    study: js

---
+++ python/for4.py
@@ -0,0 +1,5 @@
+user_input = input("횟수를 입력해주세요")
+num = int(user_input)
+
+for i in range (num) :
+    print("안녕")
---
+++ python/for5.py
@@ -0,0 +1,6 @@
+user_input = input("숫자를 입력해주세요")
+
+num = int(user_input)
+
+for i in range (1, num+1) :
+    print(i)
