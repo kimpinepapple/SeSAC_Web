dc35dfbc2e71f76c2683ab1b482079f36c1a1033
kimpinepapple <gohigher96@gmail.com>
2022-09-28 17:16:34 +0900

    Study: react JSX State Event

---
+++ sesac/week11/220923/study/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter'
+import Say from './Say'
+import StatePractice1 from './StatePractice1'
+import StatePractice2 from './StatePractice2';
+import EventClass1 from './EventClass1';
+import EventClass2 from './EventClass2';
+
+const App = () => {
+    return <EventClass1 />
+};
+
+export default App;
